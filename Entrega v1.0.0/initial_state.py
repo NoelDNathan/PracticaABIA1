8789880be904e0e360fce1564d699d9a1f8b3901
Noel Nathan <NoelDNathan@gmail.com>
2022-10-25 23:33:29 +0200

    add empty initial state

--- Entrega v1.0.0/initial_state.py
+++ Entrega v1.0.0/initial_state.py
@@ -7,22 +7,30 @@ def distance(obj1, obj2):
     y = obj1.CoordY - obj2.CoordY
     return (x**2 + y ** 2)**(1/2)
 
+
 def electry_supplied_to_client(client: Cliente, power_plant: Central) -> int:
     dist = distance(client, power_plant)
     return client.Consumo * (1 + VEnergia.loss(dist))
 
 
 class InitialState(object):
+    @staticmethod
+    def empty_state(params: ProblemParameters):
+        remain, consum = InitialState.remain_consum(params)
+        n = len(params.clients_vector)
+        c_pp = [NONPOWERPLANT for _ in range(n)]
+        gain, prices = InitialState.calculate_gain(params, c_pp, remain)
+        return StateRepresentation(params, c_pp, remain, consum, gain, prices)
 
     @staticmethod
     def simple_state(params, asc=True) -> StateRepresentation:
         remain, consum = InitialState.remain_consum(params)
-        
-        c_pp  = []
-        pp    = list(range(len(params.power_plants_vector)))
+
+        c_pp = []
+        pp = list(range(len(params.power_plants_vector)))
         if asc:
             pp.reverse()
-        
+
         id_pp = pp.pop()
         for id_client, client in enumerate(params.clients_vector):
             if client.Contrato == NOGARANTIZADO:
@@ -43,10 +51,10 @@ class InitialState(object):
                         pp.reverse()
 
         c_pp = np.array(c_pp)
-        
+
         gain, prices = InitialState.calculate_gain(params, c_pp, remain)
         return StateRepresentation(params, c_pp, remain, consum, gain, prices)
-    
+
     @staticmethod
     def remain_consum(params):
         remain = []
@@ -71,21 +79,24 @@ class InitialState(object):
 
         for id_pp, pp in enumerate(params.power_plants_vector):
             prices[0].append(VEnergia.stop_cost(pp.Tipo))
-            prices[1].append(VEnergia.daily_cost(pp.Tipo) + VEnergia.costs_production_mw(pp.Tipo) * pp.Produccion)
+            prices[1].append(VEnergia.daily_cost(
+                pp.Tipo) + VEnergia.costs_production_mw(pp.Tipo) * pp.Produccion)
 
-            if pp.Produccion == remain[id_pp]: 
+            if pp.Produccion == remain[id_pp]:
                 gain -= prices[0][id_pp]
             else:
                 gain -= prices[1][id_pp]
 
         for id_c, c in enumerate(params.clients_vector):
-            if c.Contrato == 0:   
+            if c.Contrato == 0:
                 gain += c.Consumo * VEnergia.tarifa_cliente_garantizada(c.Tipo)
-            else:                 
-                gain += c.Consumo * VEnergia.tarifa_cliente_no_garantizada(c.Tipo)
+            else:
+                gain += c.Consumo * \
+                    VEnergia.tarifa_cliente_no_garantizada(c.Tipo)
 
-            prices[2].append(c.Consumo * VEnergia.tarifa_cliente_penalizacion(c.Tipo))
-            if c_pp[id_c] == NONPOWERPLANT:  gain -= prices[2][id_c]
+            prices[2].append(
+                c.Consumo * VEnergia.tarifa_cliente_penalizacion(c.Tipo))
+            if c_pp[id_c] == NONPOWERPLANT:
+                gain -= prices[2][id_c]
 
         return gain, prices
-            
